2cc99ced7cb1690d0f62647b086a9a66d684c731
Saeed Rezaee <saeed.rezaee@kynetics.it>
2024-08-13 14:02:51 +0200

    Update Copyright (Company name)
    Changed company name from Kynetics LLC to Kynetics, Inc.
    
    Signed-off-by: Saeed Rezaee <saeed.rezaee@kynetics.it>

--- build.gradle
+++ build.gradle
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017-2024  Kynetics  LLC
+ * Copyright © 2017-2024  Kynetics, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
